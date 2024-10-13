5a51a39f87b62aa0cc4ce6b3d9d71689a0c540cd
c4ffein <c4ffein@gmail.com>
2024-10-13 23:20:01 +0200

    home-page first modifications

--- front/src/app/home-page.jsx
+++ front/src/app/home-page.jsx
@@ -1,22 +1,19 @@
-'use client';
-
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import React from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import dynamic from 'next/dynamic';
-import classNames from 'classnames';
-import { useDebounceValue } from 'usehooks-ts';
+import { Link, useRouter, useLocation } from "wouter";
 import Header from '../components/header';
 import Sidebar from '../components/sidebar';
 import Icon from '../components/icon';
 import Spinner from '../components/spinner';
 import { formatByVersion } from '../lib/api';
+import { debounce } from '../lib/helpers';
+import useIsMounted from '../lib/use-is-mounted';
 
-const Editor = dynamic(() => import('../components/editor', { ssr: false }));
+import Editor from '../components/editor';
 
 export default function HomePage({ props }) {
-  const router = useRouter();
+  const router = useRouter(); // TODO : DELETEME? Check what was the purpose in the first place
+  const [location, navigate] = useLocation();
 
   const [version, setVersion] = useState(props.version);
   const [source, setSource] = useState(props.source);
@@ -26,42 +23,39 @@ export default function HomePage({ props }) {
   const [state, setState] = useState(props.state);
   const [issueLink, setIssueLink] = useState(props.issueLink);
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
-  const [debouncedSource] = useDebounceValue(source, 100);
-  const [debouncedOptions] = useDebounceValue(options, 100);
-  const [debouncedVersion] = useDebounceValue(version, 100);
+  const inert = useMemo(() => ({source, version, options}), []);  // dirty trick
+  const isMounted = useIsMounted();
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      if (localStorage.getItem('sidebar:visible') === 'true') {
-        setIsSidebarVisible(true);
-      }
-    }
+    if (typeof window === 'undefined' || localStorage.getItem('sidebar:visible') !== 'true') return;
+    setIsSidebarVisible(true);
   }, []);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('sidebar:visible', isSidebarVisible);
-    }
+    if (typeof window !== 'undefined') { localStorage.setItem('sidebar:visible', isSidebarVisible); }
   }, [isSidebarVisible]);
 
   useEffect(() => {
-    const href = `/?version=${version}&state=${state}`;
-    router.replace(href, { scroll: false });
-  }, [router, version, state]);
+    navigate(`/?version=${version}&state=${state}`, { replace: true, scroll: false });  // TODO : Check scroll
+  }, [router, version, state]);  // TODO : Do something with router to make it work on change?.. Just remove router?
 
-  useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      const json = await formatByVersion(debouncedVersion, {
-        source: debouncedSource,
-        options: debouncedOptions,
-      });
-      setIsLoading(false);
-      setFormatted(json.formatted_code);
-      setState(json.state);
-      setIssueLink(json.issue_link);
-    })();
-  }, [debouncedSource, debouncedVersion, debouncedOptions]);
+  const formatCallback = useMemo(
+    () => debounce(
+      async () => {
+        if (!isMounted.current) return;
+        setIsLoading(true);
+        const json = await formatByVersion(inert.version, {source: inert.source, options: inert.options});
+        if (!isMounted.current) return;
+        setIsLoading(false);
+        setFormatted(json.formatted_code);
+        setState(json.state);
+        setIssueLink(json.issue_link);
+      },
+      100,
+    ),
+    []
+  );
+  useEffect(() => { Object.assign(inert, {source, version, options}); formatCallback(); }, [source, version, options]);
 
   const handleOptionsUpdate = async (value) => {
     if (value.version) {
@@ -71,13 +65,11 @@ export default function HomePage({ props }) {
     }
   };
 
-  const handleSourceUpdate = (value) => {
-    setSource(value);
-  };
+  const handleSourceUpdate = (value) => { setSource(value); };
+  const handleToggleSidebar = () => { setIsSidebarVisible((prev) => !prev); };
 
-  const handleToggleSidebar = () => {
-    setIsSidebarVisible((prev) => !prev);
-  };
+  const moreButtonClasses = isSidebarVisible ? 'text-black hover:text-slate-500' : 'text-slate-500 hover:text-black';
+  const buttonClasses = `text-sm inline-flex items-center ${moreButtonClasses}`
 
   return (
     <div className="flex flex-col h-screen">
@@ -95,40 +87,20 @@ export default function HomePage({ props }) {
 
           <div className="flex flex-1">
             <div className="flex flex-1 relative">
-              <Editor
-                value={source}
-                marginColumn={options.line_length}
-                onChange={handleSourceUpdate}
-              />
+              <Editor value={source} marginColumn={options.line_length} onChange={handleSourceUpdate} />
             </div>
             <div className="flex flex-1 relative">
               {isLoading ? (
-                <div className="flex items-center justify-center w-full ace-tomorrow-night">
-                  <Spinner />
-                </div>
+                <div className="flex items-center justify-center w-full ace-tomorrow-night"><Spinner /></div>
               ) : (
-                <Editor
-                  value={formatted}
-                  marginColumn={options.line_length}
-                  readOnly={true}
-                />
+                <Editor value={formatted} marginColumn={options.line_length} readOnly={true} />
               )}
             </div>
           </div>
         </div>
 
         <div className="flex justify-between content-center items-center p-4">
-          <button
-            className={classNames('text-sm inline-flex items-center', {
-              'text-black': isSidebarVisible,
-              'hover:text-slate-500': isSidebarVisible,
-              'text-slate-500': !isSidebarVisible,
-              'hover:text-black': !isSidebarVisible,
-            })}
-            onClick={handleToggleSidebar}
-          >
-            <Icon icon="cog" />
-          </button>
+          <button className={buttonClasses} onClick={handleToggleSidebar}><Icon icon="cog" /></button>
           <div className="flex text-right">
             <div className="flex text-right">
               <Link

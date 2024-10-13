9beb00476bda3cf274739732f42a84976c127dd8
c4ffein <c4ffein@gmail.com>
2024-10-13 23:20:41 +0200

    layout temp quickfix

--- front/src/app/layout.jsx
+++ front/src/app/layout.jsx
@@ -1,4 +1,5 @@
-import { Inter } from 'next/font/google';
+// TODO : Remove whole?
+// TODO import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -27,7 +28,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <!-- TODO body className={inter.className}>{children}</body-->
+      <body>{children}</body>
     </html>
   );
 }
